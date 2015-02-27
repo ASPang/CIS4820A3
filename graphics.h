97e8982828af61f7017a4ee6f32f47dc3e4f7953
aspang <apang@uoguelph.ca>
2015-02-27 16:55:50 -0500

    Updated assignment 3 base files with things implemented in assignment 1 and 2

---
+++ graphics.h
@@ -0,0 +1,40 @@
+
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+
+        /* world size and storage array */
+#define WORLDX 100
+#define WORLDY 50
+#define WORLDZ 100
+GLubyte  world[WORLDX][WORLDY][WORLDZ];
+
+#define MAX_DISPLAY_LIST 500000
+
+/*Functions for CIS4820 Assignment 1*/
+int climbCube(int, int, int);
+void gameWall();
+
+void gravity();
+void avoidCubeEdge();
+
+void landscape();
+void grassLand();
+void waterFlow();
+void mountainTops();
+void perlinNoise();
+void fillMountain(int, int, int);
+
+void cloudFloat();
+void cloudShape();
+void createCloud(int, int, int, int *, int);
+void moveCloud();
+void updateCloudLoc();
+
+
+/*Functions for CIS4820 Assignment 2*/
+void updateProjectiles();
+void worldOrientation();
+
+float calSpeed(int);
+float calAngle(int);
